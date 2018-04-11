d6cea5662d59d8c4dfe940d35a4555f5892e420c
mmhohenb <ninjasstillexist@gmail.com>
2018-04-11 14:52:21 -0400

    Add files via upload

---
+++ AuthorshipVerification.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 11 14:37:55 2018
+
+@author: Mercedes
+
+L690 Homework 7; mini- PAN 2015 Author Identification Task
+"""
+"""
+Goal: create a Y/N classification system which identifies whether or not the same person wrote two given documents.
+
+General StepOrder:
+
+Pre1. Determine Features
+Pre2. Code Features    
+1. For each set of documents: Reference the two documents, which are in the same folder, to the code.
+2. Get values for the features.
+3. Weigh the features.
+4. Spit out (Y/N) answer.
+5. Compare machine's answer to the truth.txt answer: Precision, Recall, Accuracy
+"""
